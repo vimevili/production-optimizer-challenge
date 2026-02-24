ea2b128414c5e7255248ecfd3c1c9454089aff42
vimevili <vinicius_meirelless@hotmail.com>
2026-02-24 09:51:09 -0300

    feat: initialize production optimizer application with Spring Boot and Vue 3

---
+++ backend/src/main/java/com/projedata/production_optimizer/ProductionOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.projedata.production_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionOptimizerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projedata/production_optimizer/ProductionOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projedata.production_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7e6b6c8e534fd548eaf97ac319828e527b7246ed
vimevili <vinicius_meirelless@hotmail.com>
2026-02-26 15:51:21 -0300

    feat: define core domain models and repository layer

--- backend/src/main/java/com/projedata/production_optimizer/repositories/MaterialRepository.java
+++ backend/src/main/java/com/projedata/production_optimizer/repositories/MaterialRepository.java
@@ -3,7 +3,9 @@ package com.projedata.production_optimizer.repositories;
 import com.projedata.production_optimizer.models.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MaterialRepository extends JpaRepository<Material, UUID> {
+    Optional<Material> findByName(String name);
 }
